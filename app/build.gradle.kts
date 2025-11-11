1965faf3c8cd7694fde3898d74daea46604fe86c
somanisushant23 <sushant@godatingapp.com>
2025-11-11 11:16:28 +0530

    FIxing update bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
 
+    // SwipeRefreshLayout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
